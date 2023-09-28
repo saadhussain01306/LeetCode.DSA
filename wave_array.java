da8e76799a3f765c28dc9fe2b24634f30a2e4a12
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2023-09-28 20:21:49 +0530

    Create wave_array.java

---
+++ wave_array.java
@@ -0,0 +1,13 @@
+//wave array 
+// we swap the adjacent array values
+class Solution {
+    public static void convertToWave(int n, int[] a) {
+        for(int i=0;i<n-1;i+=2){
+            int temp=a[i];
+            a[i]=a[i+1];
+            a[i+1]=temp;
+        }
+        
+    }
+}
+  
