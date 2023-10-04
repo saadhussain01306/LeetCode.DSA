581ccbb0d0adf2f2a237080faad19c24da8e3fbf
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2023-10-04 22:21:02 +0530

    Create third_maximum.java

---
+++ third_maximum.java
@@ -0,0 +1,51 @@
+/*Given an integer array nums, return the third distinct maximum number in this array. 
+If the third maximum does not exist, return the maximum number.
+Example 1:
+
+Input: nums = [3,2,1]
+Output: 1
+Explanation:
+The first distinct maximum is 3.
+The second distinct maximum is 2.
+The third distinct maximum is 1.
+Example 2:
+
+Input: nums = [1,2]
+Output: 2
+Explanation:
+The first distinct maximum is 2.
+The second distinct maximum is 1.
+The third distinct maximum does not exist, so the maximum (2) is returned instead.
+Example 3:
+
+Input: nums = [2,2,3,1]
+Output: 1
+Explanation:
+The first distinct maximum is 3.
+The second distinct maximum is 2 (both 2's are counted together since they have the same value).
+The third distinct maximum is 1.
+*/
+  
+class Solution {
+    public int thirdMax(int[] nums) {
+        long m1 = Long.MIN_VALUE;
+        long m2 = Long.MIN_VALUE;
+        long m3 = Long.MIN_VALUE;
+        for (int num : nums) {
+            if (num == m1 || num == m2 || num == m3) {
+                continue;
+            }
+            if (num > m1) {
+                m3 = m2;
+                m2 = m1;
+                m1 = num;
+            } else if (num > m2) {
+                m3 = m2;
+                m2 = num;
+            } else if (num > m3) {
+                m3 = num;
+            }
+        }
+        return (int) (m3 == Long.MIN_VALUE ? m1 : m3);
+    }
+}
