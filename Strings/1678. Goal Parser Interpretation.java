7a9553cca657751cd37ea13a401bded2927de6ff
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2023-12-09 20:22:55 +0530

    Create 1678. Goal Parser Interpretation.java

---
+++ Strings/1678. Goal Parser Interpretation.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String interpret(String command) {
+        StringBuilder ans =new StringBuilder();
+        for(int i=0;i<command.length();i++){
+            char c=command.charAt(i);
+            if(c=='G'){
+                ans.append('G');
+            }
+            else if(c=='('){
+                ans.append(command.charAt(i+1)==')'? "o" :"al");
+            }
+        } 
+        return ans.toString();
+
+    }
+}
