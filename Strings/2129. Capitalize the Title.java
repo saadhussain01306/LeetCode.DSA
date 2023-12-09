88884e9e11a3183da451f103c70393a711dbaab3
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2023-12-09 19:56:18 +0530

    Create 2129. Capitalize the Title.java

---
+++ Strings/2129. Capitalize the Title.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String capitalizeTitle(String title) {
+        ArrayList<String> ans= new ArrayList<>();
+        for(String s:title.split(" ")){
+            if(s.length()<3){
+                ans.add(s.toLowerCase());
+            }
+            else{
+                ans.add(s.substring(0,1).toUpperCase()+
+                s.substring(1).toLowerCase());
+            }
+        }
+        return String.join(" ",ans);
+    }
+}
