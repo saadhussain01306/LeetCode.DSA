2eacb92d8b147195af6dad06d8cd8a5b71cf2e63
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2024-01-27 00:17:28 +0530

    Create 2073. Time Needed to Buy Tickets.java

---
+++ Arrays/2073. Time Needed to Buy Tickets.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int len=tickets.length;
+        int time=0;
+
+    while(tickets[k]!=0){
+          for(int i=0;i<len;i++){
+              if(tickets[i]!=0 && tickets[k]!=0){
+                  tickets[i]-=1;
+                  time++;
+              }
+          }
+    }
+
+    return time;
+
+    }
+}
