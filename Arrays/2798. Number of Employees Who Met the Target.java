1f7ffe0e8b621eeb989e69373df8fbbbeca132b9
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2024-02-12 16:05:49 +0530

    Create 2798. Number of Employees Who Met the Target.java

---
+++ Arrays/2798. Number of Employees Who Met the Target.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int len=hours.length;
+        int count=0;
+        for(int i=0;i<len;i++){
+           if(hours[i]>=target){
+               count++;
+           } 
+           else{
+               continue;
+           }
+        }
+
+        return count;
+    }
+}
