87edb35fb6353772bbd11c591b7c07ea974f79b6
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2024-03-03 17:38:06 +0530

    Create 118. Pascal's Triangle.java

---
+++ Lists/118. Pascal's Triangle.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+          List<List<Integer>> ans= new ArrayList<>();
+
+          List<Integer> first_row=new ArrayList<>();
+          first_row.add(1);
+          ans.add(first_row);
+
+          for(int i=1;i<numRows;i++){
+              List<Integer> prev= ans.get(i-1);
+              List<Integer> cur=new ArrayList<>();
+              cur.add(1);
+
+              for(int j=0;j<i-1;j++){
+                 cur.add(prev.get(j)+prev.get(j+1));
+              }
+
+              cur.add(1);
+
+              ans.add(cur);
+          }
+
+          return ans;
+    }
+}
