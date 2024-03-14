996b2674846881efd5ed1e29bac4034aee54c62b
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2024-03-14 23:33:15 +0530

    Create 560. Subarray Sum Equals K.java

---
+++ Two-pointer/560. Subarray Sum Equals K.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int n = nums.length;
+        
+        for (int start = 0; start < n; start++) {
+            int sum = 0;
+            for (int end = start; end < n; end++) {
+                sum += nums[end];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
