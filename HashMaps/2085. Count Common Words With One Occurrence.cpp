55584e62ba97d09df1104e7e347da6b924ee4b18
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2024-08-05 12:35:30 +0530

    Create 2085. Count Common Words With One Occurrence.cpp

---
+++ HashMaps/2085. Count Common Words With One Occurrence.cpp
@@ -0,0 +1,66 @@
+/*
+Given two string arrays words1 and words2, return the number of strings that appear exactly once in each of the two arrays.
+
+ 
+
+Example 1:
+
+Input: words1 = ["leetcode","is","amazing","as","is"], words2 = ["amazing","leetcode","is"]
+Output: 2
+Explanation:
+- "leetcode" appears exactly once in each of the two arrays. We count this string.
+- "amazing" appears exactly once in each of the two arrays. We count this string.
+- "is" appears in each of the two arrays, but there are 2 occurrences of it in words1. We do not count this string.
+- "as" appears once in words1, but does not appear in words2. We do not count this string.
+Thus, there are 2 strings that appear exactly once in each of the two arrays.
+Example 2:
+
+Input: words1 = ["b","bb","bbb"], words2 = ["a","aa","aaa"]
+Output: 0
+Explanation: There are no strings that appear in each of the two arrays.
+Example 3:
+
+Input: words1 = ["a","ab"], words2 = ["a","a","a","ab"]
+Output: 1
+Explanation: The only string that appears exactly once in each of the two arrays is "ab".
+ 
+
+Constraints:
+
+1 <= words1.length, words2.length <= 1000
+1 <= words1[i].length, words2[j].length <= 30
+words1[i] and words2[j] consists only of lowercase English letters.
+
+*/
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int countWords(vector<string>& words1, vector<string>& words2) {
+       unordered_map<string, int> map1;
+       unordered_map<string, int> map2;
+
+       for(int i=0;i<words1.size();i++){
+        map1[words1[i]]++;
+       }
+
+       for(int i=0;i<words2.size();i++){
+        map2[words2[i]]++;
+       }
+      int count=0;
+       for(auto str:words1){
+           if(map1[str]==1 && map2[str]==1){
+            count++;
+           }
+       }
+
+       return count;
+
+    }
+};
